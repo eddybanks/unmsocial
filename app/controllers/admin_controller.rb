bd664c9421af8ab7eb37f138124d03ac99c3da75
Edwin Agbenyega <eddybanks03@gmail.com>
2015-04-21 19:20:53 -0600

    minor changes

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,10 +1,15 @@
 class AdminController < ApplicationController
 
   def index
-    @users = User.all
-    @events = Event.all
-    @groups = Group.all
-    @contacts = Contact.all
+    if current_user.username == 'eddybanks'
+      @users = User.all
+      @events = Event.all
+      @groups = Group.all
+      @contacts = Contact.all
+    else
+      flash[:notice] = "Not admin user"
+      redirect_to root_path
+    end
   end
 
 end

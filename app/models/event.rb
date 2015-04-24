bae6e7f8f72d679e348415e59d1f89249df0fc3d
Edwin Agbenyega <eddybanks03@gmail.com>
2015-04-24 13:57:19 -0600

    minor changes

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,6 +2,8 @@ class Event < ActiveRecord::Base
 
   has_and_belongs_to_many :groups
   has_and_belongs_to_many :users
+  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
   validates :name, presence: true
 
   scope :sorted, lambda { order("events.name ASC")}

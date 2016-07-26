7c6e6556a57b6370b0ef116071a0d4949fe3dc64
Edwin Agbenyega <eddybanks03@gmail.com>
2016-07-25 23:45:08 -0600

    site dependencies outdated

--- Gemfile
+++ Gemfile
@@ -1,9 +1,9 @@
 source 'https://rubygems.org'
 
 
-gem 'rails', '4.2.0'
+gem 'rails', '4.2.4'
 gem 'bootstrap-sass'
-gem 'mysql2'
+gem 'pg'
 gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.1.0'
@@ -29,6 +29,5 @@ group :development, :test do
 end
 
 group :production do
-  gem 'pg'
   gem 'rails_12factor'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -13,53 +13,56 @@
 
 ActiveRecord::Schema.define(version: 20150421233505) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "contacts", force: :cascade do |t|
-    t.string   "name",       limit: 255
-    t.text     "message",    limit: 65535
-    t.datetime "created_at",               null: false
-    t.datetime "updated_at",               null: false
-    t.string   "email",      limit: 255
+    t.string   "name"
+    t.text     "message"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string   "email"
   end
 
   create_table "events", force: :cascade do |t|
-    t.string   "name",                limit: 255
-    t.string   "venue",               limit: 255
+    t.string   "name"
+    t.string   "venue"
     t.datetime "time"
-    t.integer  "capacity",            limit: 4
+    t.integer  "capacity"
     t.string   "phone_number",        limit: 20
-    t.datetime "created_at",                        null: false
-    t.datetime "updated_at",                        null: false
-    t.string   "category",            limit: 255
-    t.text     "details",             limit: 65535
-    t.string   "avatar_file_name",    limit: 255
-    t.string   "avatar_content_type", limit: 255
-    t.integer  "avatar_file_size",    limit: 4
+    t.datetime "created_at",                     null: false
+    t.datetime "updated_at",                     null: false
+    t.string   "category"
+    t.text     "details"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
-    t.integer  "created_by",          limit: 4
+    t.integer  "created_by"
   end
 
   add_index "events", ["name"], name: "index_events_on_name", using: :btree
   add_index "events", ["time"], name: "index_events_on_time", using: :btree
 
   create_table "events_groups", id: false, force: :cascade do |t|
-    t.integer "event_id", limit: 4
-    t.integer "group_id", limit: 4
+    t.integer "event_id"
+    t.integer "group_id"
   end
 
   add_index "events_groups", ["event_id", "group_id"], name: "index_events_groups_on_event_id_and_group_id", using: :btree
 
   create_table "events_users", id: false, force: :cascade do |t|
-    t.integer "event_id", limit: 4
-    t.integer "user_id",  limit: 4
+    t.integer "event_id"
+    t.integer "user_id"
   end
 
   add_index "events_users", ["event_id", "user_id"], name: "index_events_users_on_event_id_and_user_id", using: :btree
 
   create_table "faqcomments", force: :cascade do |t|
-    t.integer  "user_id",    limit: 4
-    t.text     "comment",    limit: 65535
-    t.datetime "created_at",               null: false
-    t.datetime "updated_at",               null: false
+    t.integer  "user_id"
+    t.text     "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_index "faqcomments", ["user_id"], name: "index_faqcomments_on_user_id", using: :btree
@@ -68,24 +71,24 @@ ActiveRecord::Schema.define(version: 20150421233505) do
     t.string   "group_name",          limit: 50
     t.string   "category",            limit: 50
     t.string   "phone_number",        limit: 20
-    t.text     "details",             limit: 65535
-    t.datetime "created_at",                        null: false
-    t.datetime "updated_at",                        null: false
+    t.text     "details"
+    t.datetime "created_at",                     null: false
+    t.datetime "updated_at",                     null: false
     t.time     "time"
-    t.string   "venue",               limit: 255
-    t.string   "avatar_file_name",    limit: 255
-    t.string   "avatar_content_type", limit: 255
-    t.integer  "avatar_file_size",    limit: 4
+    t.string   "venue"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
-    t.string   "day",                 limit: 255
-    t.integer  "created_by",          limit: 4
+    t.string   "day"
+    t.integer  "created_by"
   end
 
   add_index "groups", ["group_name"], name: "index_groups_on_group_name", using: :btree
 
   create_table "groups_users", id: false, force: :cascade do |t|
-    t.integer "group_id", limit: 4
-    t.integer "user_id",  limit: 4
+    t.integer "group_id"
+    t.integer "user_id"
   end
 
   add_index "groups_users", ["group_id", "user_id"], name: "index_groups_users_on_group_id_and_user_id", using: :btree
@@ -95,16 +98,16 @@ ActiveRecord::Schema.define(version: 20150421233505) do
     t.string   "fname",               limit: 15
     t.string   "lname",               limit: 15
     t.date     "date_of_birth"
-    t.string   "gender",              limit: 255
-    t.text     "address",             limit: 255
+    t.string   "gender"
+    t.text     "address"
     t.string   "email",               limit: 20
     t.string   "phone_number",        limit: 20
-    t.datetime "created_at",                      null: false
-    t.datetime "updated_at",                      null: false
-    t.boolean  "admin",               limit: 1
-    t.string   "avatar_file_name",    limit: 255
-    t.string   "avatar_content_type", limit: 255
-    t.integer  "avatar_file_size",    limit: 4
+    t.datetime "created_at",                     null: false
+    t.datetime "updated_at",                     null: false
+    t.boolean  "admin"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
   end
 
